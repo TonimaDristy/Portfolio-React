eb15e2562b416c3aa97d3f61b984f41da928e942
TonimaDristy <tidristy477@gmail.com>
2026-01-15 01:11:01 +0600

    header

--- portfolio-react/components/Header.tsx
+++ portfolio-react/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header>
-            Header works
+        <header className="p-6 text-center">
+            <h1 className="text-3xl font-bold">Header</h1>
         </header>
     );
 }
